0508ded5378d965dcf989d70ade0be44c570e370
Cem Asma <dev.cemas@gmail.com>
2017-02-20 19:15:09 +0300

    sort and flags written

--- analyzer.go
+++ analyzer.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
 type Word struct {
-	Key   string
-	Value int
+	Content string
+	Value   int
 }
 
 func main() {
-	byt, _ := ioutil.ReadFile("C:\\Users\\cemasma\\Desktop\\langüage öf ıngilazca ile WhatsApp Sohbeti.txt")
-	//fmt.Println(string(byt))
+	file := flag.String("file", "", "Whatsapp chat record file address.")
+	username := flag.String("username", "", "Provides specified analysis by username.")
+	limit := flag.Int("limit", 10, "It limits list from start to sended value.")
+	flag.Parse()
 
-	words := getWordsByValue(getWordsWithCounts(getWords(string(byt))))
-	fmt.Println(words)
+	byt, _ := ioutil.ReadFile(*file)
 
+	if len(*username) > 0 {
+		// TODO: User's lines will be under review.
+	} else if len(*file) > 0 {
+		words := sortWordsByCount(getWordsByValue(getWordsWithCounts(getWords(string(byt)))))
+
+		for i := 0; i < *limit; i++ {
+			fmt.Printf("Word: %s 		Count: %d\n", words[i].Content, words[i].Value)
+		}
+	}
 }
 
 func getWords(chatRecord string) []string {
@@ -38,6 +49,7 @@ func getWordsWithCounts(words []string) map[string]int {
 	wordsWithCounts := make(map[string]int)
 
 	for _, value := range words {
+		value = strings.ToLower(value)
 		if _, ok := wordsWithCounts[value]; ok {
 			wordsWithCounts[value]++
 		} else {
@@ -55,4 +67,28 @@ func getWordsByValue(wordsWithCounts map[string]int) map[int][]string {
 	}
 
 	return wordsByCounts
+}
+
+func sortWordsByCount(wordsByCount map[int][]string) []Word {
+	words := []Word{}
+
+	for count, wordArr := range wordsByCount {
+		for _, word := range wordArr {
+			if word != "" && word != "atlanmış>" && word != "<medya" {
+				words = append(words, Word{Content: word, Value: count})
+			}
+		}
+	}
+
+	for i := 0; i < len(words); i++ {
+		for j := i + 1; j < len(words); j++ {
+			if words[i].Value < words[j].Value {
+				backup := words[i]
+				words[i] = words[j]
+				words[j] = backup
+			}
+		}
+	}
+
+	return words
 }
