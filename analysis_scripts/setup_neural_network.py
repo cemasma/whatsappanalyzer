767282aeeeb07a0525b32af9ee242caaacf2dfa6
Cem Asma <cem.asma@getir.com>
2020-06-02 00:44:25 +0300

    stemmer and unccessary functions removed for setup neural network

--- analysis_scripts/setup_neural_network.py
+++ analysis_scripts/setup_neural_network.py
@@ -1,7 +1,7 @@
 # use natural language toolkit
 import nltk
 nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
+# from nltk.stem.lancaster import LancasterStemmer
 #from TurkishStemmer import TurkishStemmer
 import os
 import json
@@ -9,7 +9,7 @@ import datetime
 import numpy as np
 import time
 import sys
-stemmer = LancasterStemmer()
+# stemmer = LancasterStemmer()
 #stemmer = TurkishStemmer()
 
 analysis_type = 'sentiment'
@@ -107,41 +107,6 @@ def sigmoid(x):
 # convert output of sigmoid function to its derivative
 def sigmoid_output_to_derivative(output):
     return output*(1-output)
- 
-def clean_up_sentence(sentence):
-    # tokenize the pattern
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
-
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-def bow(sentence, words, show_details=False):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-
-    return(np.array(bag))
-
-def think(sentence, show_details=False):
-    x = bow(sentence.lower(), words, show_details)
-    if show_details:
-        print ("sentence:", sentence, "\n bow:", x)
-    # input layer is our bag of words
-    l0 = x
-    # matrix multiplication of input and hidden layer
-    l1 = sigmoid(np.dot(l0, synapse_0))
-    # output layer
-    l2 = sigmoid(np.dot(l1, synapse_1))
-    return l2
-
 
 # ANN and Gradient Descent code from https://iamtrask.github.io//2015/07/27/python-network-part2/
 def train(X, y, hidden_neurons=10, alpha=1, epochs=50000, dropout=False, dropout_percent=0.5):
